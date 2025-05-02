6c5f66263b132b322da0091bf4d23c6de21433de
corinB <gkdisrha2020@gmail.com>
2025-05-02 15:39:23 +0900

    class3

---
+++ study/src/solvAC/class3/Hide.java
@@ -0,0 +1,61 @@
+package solvAC.class3;
+/*수빈이는 동생과 숨바꼭질을 하고 있다. 수빈이는 현재 점 N(0 ≤ N ≤ 100,000)에 있고, 동생은 점 K(0 ≤ K ≤ 100,000)에 있다.
+수빈이는 걷거나 순간이동을 할 수 있다. 만약, 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다.
+순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동하게 된다.
+수빈이와 동생의 위치가 주어졌을 때, 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인지 구하는 프로그램을 작성하시오.*/
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+//no.1697
+public class Hide {
+    static int N;
+    static int K;
+    static int[] check = new int[100001];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+
+        if (N == K) {
+            System.out.println(0);
+        } else {
+            bfs(N);
+        }
+    }
+
+    static void bfs(int num) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(num);
+        check[num] = 1;
+
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+
+            for (int i = 0; i < 3; i++) {
+                int next;
+
+                if (i == 0) {
+                    next = temp + 1;
+                } else if (i == 1) {
+                    next = temp - 1;
+                } else {
+                    next = temp * 2;
+                }
+
+                if (next == K) {
+                    System.out.println(check[temp]);
+                    return;
+                }
+
+                if (next >= 0 && next < check.length && check[next] == 0) {
+                    q.add(next);
+                    check[next] = check[temp] + 1;
+                }
+            }
+        }
+    }
+}
